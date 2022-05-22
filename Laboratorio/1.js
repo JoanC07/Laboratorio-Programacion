87d0d8c9be15fcfa4d8bf3c4ceb28a7b1f5d9e85
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-05-22 15:14:18 -0500

    Joan

--- Laboratorio/1.js
+++ Laboratorio/1.js
@@ -5,53 +5,72 @@ class EncabezadoHeader extends HTMLElement{
     }
     connectedCallback(){
       this.innerHTML=`
-      <header class="header">
-      <div class="container logo-nav-container">
-         <div class="logo" align="center"><img src="·">
-              <nav class="navegation">
-                  <ul>
-                      <li><a href="#">INICIO</a></li>
-                      <li><a href="#">DESCRIPCION</a></li>
-                      <li><a href="#">NUESTRA GALERIA</a></li>
-                      <li><a href="#">SUGERENCIAS</a></li>
-                      <li><a href="#">CONTACTENOS</a></li>
-                  </ul>
-              </nav>
-          </div>
-      </header>
+      <header>
+    <nav>
+        <a href="S1.html">INICIO</a>
+        <a href="S2.html">DESCRIPCIÓN</a>
+        <a href="S3.html">NUESTRA GALERIA</a>
+        <a href="S4.html">SUGERENCIAS</a>
+        <a href="S5.html">CONTACTENOS</a>
+    </nav>
+    <section class="textos-header">
+        <h1>Lo mejor de| ECUADOR</h1>
+        <h2>Universidad ESPE</h2>
+    </section>
+    <div class="wave" style="height: 150px; overflow: hidden;">
+        </svg></div>
+</header>
+
+    <style>
+    header {
+      width: 100%;
+      height: 500px;
+      background: #ffffff;
+      background: -webkit-linear-gradient(to right, hsla(224, 100%, 67%, 0.459), hsla(224, 83%, 72%, 0.664)), url(Imagenes/portada.jpg);
+      background: linear-gradient(to right, hsla(222, 93%, 53%, 0.459), hsla(219, 86%, 75%, 0.664)), url(Imagenes/portada.jpg);
+      background-size: cover;
+      background-attachment: fixed;
+      position: relative;
+  }
   
-      <style>
-       .container {
-          width: 85%;
-          max-width: 1100px;
-          margin: 0 auto;
-      }
-       .header a {
-          color: black;
-          text-decoration: none;
-      }
-      .navegation ul {
-          margin: 0;
-          padding: 0;
-          list-style: none;
-      }
-      
-      .navegation ul li {
-          display: inline-block;
-      }
-      
-      .navegation ul li a {
-          display: block;
-          padding: 0.5rem 1rem;
-          transition: all 0.4s linear;
-          font-size: 15px;
-      }
-      
-      .navegation ul li a:hover {
-          background: rgb(131, 154, 228);
-      }
-      
-    </style>
+  nav{
+      text-align: right;
+      padding: 30px 50px 0 0;
+  }
+  
+  nav > a{
+      color:#fff;
+      font-weight: 300;
+      text-decoration: none;
+      margin-right: 10px;
+  }
+  
+  nav > a:hover{
+      text-decoration: underline;
+  }
+  
+  header .textos-header{
+      display: flex;
+      height: 430px;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      text-align: center;
+  }
+  
+  .textos-header h1{
+      font-size: 50px;
+      color:#fff;
+  }
+  
+  .textos-header h2{
+      font-size: 30px;
+      font-weight: 300;
+      color:#fff;
+  }
+    
+  </style>
       `;
     }
   }
